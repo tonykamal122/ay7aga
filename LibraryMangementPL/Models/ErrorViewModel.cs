c6025817a4bdd909d642743e0a958ca93b010897
macbook <tomykamall123@gmail.com>
2025-10-24 18:07:24 +0300

    tEST

---
+++ LibraryMangementPL/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LibraryMangementPL.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
